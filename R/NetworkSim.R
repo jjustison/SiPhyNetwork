0b15fd5411a71c14e74db44aa68b34e56f2ea7d2
Justison <justison@iastate.edu>
2022-04-13 14:17:19 -0500

    update documentation for github actions

--- R/NetworkSim.R
+++ R/NetworkSim.R
@@ -44,7 +44,7 @@ getReconstructedTips<- function(tip.labels,extinct_labels){
 #' @examples
 #' set.seed(23) ##set seed with the smallest Pillai prime
 #' net<-sim.bdh.age(1,1,3,2,0.125,c(1/3,1/3,1/3),hyb.inher.fxn = make.uniform.draw(),complete = FALSE)[[1]]
-#' net<-incompleteSampling(net,0.5,stochastic=F) ##randomly sample half of the extant taxa
+#' net<-incompleteSampling(net,0.5,stochastic=FALSE) ##randomly sample half of the extant taxa
 incompleteSampling<-function(net,rho,stochastic=FALSE){
   extinct_tips <- getReconstructedTips(1:length(net$tip.label),get.extinct(net))
   if(length(extinct_tips)!=0){
